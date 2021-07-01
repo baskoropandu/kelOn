cd0d20a84631bbd215cdb24787d87ebf871d3d2b
baskoropandu <baskoro.p.g@gmail.com>
2021-07-02 01:41:14 +0700

    test

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const session = require('express-session');
 const app = express()
 const port = process.env.PORT || 3000
 
+// const INDEX = '/index.html';
+
+// const server = express()
+//   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
 const server = require('http').createServer(app)
 const io = require('socket.io')(server, {})
 
